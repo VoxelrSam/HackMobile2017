3370bc320eb1f746c683e61898399e7ab613dee8
Nima Chatlani <nima.u.chatlani@gmail.com>
2017-07-08 03:22:15 -0700

    Update clinicMap.js

--- clinicMap.js
+++ clinicMap.js
@@ -1,28 +1,18 @@
-function initMap() {
-  var center = {lat: 32.895770,lng: -117.193011};
-  var icon = {
-  	url:'http://www.orientini.com/1/images/blue-location-icon-location_icon.png',
-    scaledSize: new google.maps.Size(20, 20), // scaled size
-    origin: new google.maps.Point(0,0), // origin
-    anchor: new google.maps.Point(0, 0)
-  }
+function initialize() {
+  var curr_pos = {lat: 32.895770, lng: -117.193011};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: center
+    center: curr_pos,
+    zoom: 14
   });
-  var features  = [
-  {
-  	position: center
-  }
-  ]
-  features.forEach(function(features) {
-    var marker = new google.maps.Marker({
-      position: features.position,
-      icon: icon,
-      map: map
-    });
-  });
-  
+  var panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('pano'), {
+        position: curr_pos,
+        pov: {
+          heading: 34,
+          pitch: 10
+        }
+      });
+  map.setStreetView(panorama);
   var scripps_clinic = new google.maps.Marker({
     position: {lat: 32.893317, lng: -117.201080},
     map: map
